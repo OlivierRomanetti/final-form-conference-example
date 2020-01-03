36aad2f607635788fb171e36ced9abdcb1762d15
Olivier Romanetti <olivier.romanetti-extern@renault.com>
2020-01-03 17:08:45 +0100

    React final form conference exemple

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Form, Field } from 'react-final-form';
+import createDecorator from 'final-form-focus';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+const showResults = async values => {
+  await sleep(500);
+  window.alert(JSON.stringify(values, undefined, 2));
+};
+
+const focusOnError = createDecorator();
+const required = value => (value ? undefined : 'Required');
+
+const App = () => (
+  <div>
+    <h1>React Final Form</h1>
+    <Form onSubmit={showResults} decorators={[focusOnError]}>
+      {({ handleSubmit, values, submitting }) => (
+        <form onSubmit={handleSubmit}>
+          <Field
+            name="firstName"
+            id="firstName"
+            placeholder="First Name"
+            validate={required}
+          >
+            {({ input, meta, placeholder }) => (
+              <div className={meta.active ? 'active' : ''}>
+                <label htmlFor={input.id}>First Name</label>
+                <input {...input} placeholder={placeholder} />
+                {meta.error && meta.touched && <span>{meta.error}</span>}
+              </div>
+            )}
+          </Field>
+          <Field
+            name="lastName"
+            id="lastName"
+            placeholder="Last Name"
+            validate={required}
+          >
+            {({ input, meta, placeholder }) => (
+              <div className={meta.active ? 'active' : ''}>
+                <label htmlFor={input.id}>Last Name</label>
+                <input {...input} placeholder={placeholder} />
+                {meta.error && meta.touched && <span>{meta.error}</span>}
+              </div>
+            )}
+          </Field>
+          <Field
+            name="email"
+            id="email"
+            placeholder="email"
+            validate={required}
+          >
+            {({ input, meta, placeholder }) => (
+              <div className={meta.active ? 'active' : ''}>
+                <label htmlFor={input.id}>Email</label>
+                <input {...input} placeholder={placeholder} />
+                {meta.error && meta.touched && <span>{meta.error}</span>}
+              </div>
+            )}
+          </Field>
+          <button type="submit" disabled={submitting}>
+            Submit
+          </button>
+          <pre>{JSON.stringify(values, undefined, 2)}</pre>
+        </form>
+      )}
+    </Form>
+  </div>
+);
 
 export default App;
